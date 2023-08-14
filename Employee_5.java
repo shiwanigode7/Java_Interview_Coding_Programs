3c6383b0f3be9c26124546a1afedc1a43a59f57c
Shiwani Gode <41783218+shiwanigode7@users.noreply.github.com>
2023-08-14 21:04:38 +0530

    Update Employee_5.java

--- Employee_5.java
+++ Employee_5.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 // You have given list of employees, find out what is the average age of male and female employees
-public class Employee_5 {
+public class Employee {
 
 	private int id;
 	private String name;
@@ -16,7 +16,7 @@ public class Employee_5 {
 	private int yearOfJoining;
 	private double salary;
 
-	public Employee_5(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
+	public Employee(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
 			double salary) {
 		super();
 		this.id = id;
@@ -58,27 +58,27 @@ public class Employee_5 {
 
 	@Override
 	public String toString() {
-		return "Employee_5 [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
 				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]";
 	}
 
-	private static List<Employee_5> EMPLOYEE_List = new ArrayList<Employee_5>();
+	private static List<Employee> EMPLOYEE_List = new ArrayList<Employee>();
 
 	public static void main(String[] args) {
 
-		EMPLOYEE_List.add(new Employee_5(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
-		EMPLOYEE_List.add(new Employee_5(2, "Mithun", 15, Gender.MALE, Department.FINANCE, 2000, 40000));
-		EMPLOYEE_List.add(new Employee_5(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
-		EMPLOYEE_List.add(new Employee_5(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
-		EMPLOYEE_List.add(new Employee_5(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
-		EMPLOYEE_List.add(new Employee_5(6, "shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
-		EMPLOYEE_List.add(new Employee_5(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
-		EMPLOYEE_List.add(new Employee_5(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
-		EMPLOYEE_List.add(new Employee_5(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
-		EMPLOYEE_List.add(new Employee_5(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
+		EMPLOYEE_List.add(new Employee(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
+		EMPLOYEE_List.add(new Employee(2, "Mithun", 15, Gender.MALE, Department.FINANCE, 2000, 40000));
+		EMPLOYEE_List.add(new Employee(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
+		EMPLOYEE_List.add(new Employee(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
+		EMPLOYEE_List.add(new Employee(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
+		EMPLOYEE_List.add(new Employee(6, "shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
+		EMPLOYEE_List.add(new Employee(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
+		EMPLOYEE_List.add(new Employee(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
+		EMPLOYEE_List.add(new Employee(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
+		EMPLOYEE_List.add(new Employee(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
 
 		Map<Gender, Double> averageAgeOfMaleAndFemaleEmployees = EMPLOYEE_List.stream()
-				.collect(Collectors.groupingBy(Employee_5::getGender, Collectors.averagingInt(Employee_5::getAge)));
+				.collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingInt(Employee::getAge)));
 		System.out.println(averageAgeOfMaleAndFemaleEmployees);
 	}
 
