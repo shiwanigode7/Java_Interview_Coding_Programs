bbd8ae7411e05fc80cf91fbaf3490f71552f085d
Shiwani Gode <41783218+shiwanigode7@users.noreply.github.com>
2023-08-14 23:16:42 +0530

    Add files via upload

---
+++ Employee_14.java
@@ -0,0 +1,107 @@
+package com.interview.problems.java.core;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+// You have given list of employees, list down thenames of all employees in each department 
+public class Employee_14 {
+
+	private int id;
+	private String name;
+	private int age;
+	private Gender gender;
+	private Department department;
+	private int yearOfJoining;
+	private double salary;
+
+	public Employee_14(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
+			double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.department = department;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public int getYearOfJoining() {
+		return yearOfJoining;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee_14 [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]";
+	}
+
+	private static List<Employee_14> EMPLOYEE_LIST = new ArrayList<Employee_14>();
+
+	public static void main(String[] args) {
+
+		EMPLOYEE_LIST.add(new Employee_14(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
+		EMPLOYEE_LIST.add(new Employee_14(2, "Mithun", 15, Gender.MALE, Department.IT, 2000, 40000));
+		EMPLOYEE_LIST.add(new Employee_14(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
+		EMPLOYEE_LIST.add(new Employee_14(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
+		EMPLOYEE_LIST.add(new Employee_14(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
+		EMPLOYEE_LIST.add(new Employee_14(6, "shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
+		EMPLOYEE_LIST.add(new Employee_14(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
+		EMPLOYEE_LIST.add(new Employee_14(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
+		EMPLOYEE_LIST.add(new Employee_14(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
+		EMPLOYEE_LIST.add(new Employee_14(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
+
+		Map<Department, List<Employee_14>> employeeListByDepartment = EMPLOYEE_LIST.stream()
+				.collect(Collectors.groupingBy(Employee_14::getDepartment));
+
+		Set<Entry<Department, List<Employee_14>>> entrySet = employeeListByDepartment.entrySet();
+
+		for (Entry<Department, List<Employee_14>> entry : entrySet) {
+			System.out.println("=====================================");
+			System.out.println("Employees in " + entry.getKey() + ":");
+
+			List<Employee_14> employeeList = entry.getValue();
+
+			for (Employee_14 employee : employeeList) {
+				System.out.println(employee.getName());
+			}
+
+		}
+	}
+}
+
+enum Gender {
+	MALE, FEMALE
+}
+
+enum Department {
+	MARKETING, HR, SALES, FINANCE, RND, IT, MANUFACTURING, QUALITY_MANAGEMENT
+}
