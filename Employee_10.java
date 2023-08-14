3f588973c6123d3efbb23cfc0400c8a58bb4b1d7
Shiwani Gode <41783218+shiwanigode7@users.noreply.github.com>
2023-08-14 22:05:08 +0530

    Update Employee_10.java

--- Employee_10.java
+++ Employee_10.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 // You have given list of employees, find out the details of youngest male employee in the SALES department 
-public class Employee_10 {
+public class Employee {
 
 	private int id;
 	private String name;
@@ -16,7 +16,7 @@ public class Employee_10 {
 	private int yearOfJoining;
 	private double salary;
 
-	public Employee_10(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
+	public Employee(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
 			double salary) {
 		super();
 		this.id = id;
@@ -58,30 +58,30 @@ public class Employee_10 {
 
 	@Override
 	public String toString() {
-		return "Employee_10 [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
 				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]";
 	}
 
-	private static List<Employee_10> EMPLOYEE_List = new ArrayList<Employee_10>();
+	private static List<Employee> EMPLOYEE_List = new ArrayList<Employee>();
 
 	public static void main(String[] args) {
 
-		EMPLOYEE_List.add(new Employee_10(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
-		EMPLOYEE_List.add(new Employee_10(2, "Mithun", 15, Gender.MALE, Department.FINANCE, 2000, 40000));
-		EMPLOYEE_List.add(new Employee_10(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
-		EMPLOYEE_List.add(new Employee_10(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
-		EMPLOYEE_List.add(new Employee_10(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
-		EMPLOYEE_List.add(new Employee_10(6, "shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
-		EMPLOYEE_List.add(new Employee_10(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
-		EMPLOYEE_List.add(new Employee_10(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
-		EMPLOYEE_List.add(new Employee_10(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
-		EMPLOYEE_List.add(new Employee_10(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
+		EMPLOYEE_List.add(new Employee(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
+		EMPLOYEE_List.add(new Employee(2, "Mithun", 15, Gender.MALE, Department.FINANCE, 2000, 40000));
+		EMPLOYEE_List.add(new Employee(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
+		EMPLOYEE_List.add(new Employee(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
+		EMPLOYEE_List.add(new Employee(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
+		EMPLOYEE_List.add(new Employee(6, "shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
+		EMPLOYEE_List.add(new Employee(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
+		EMPLOYEE_List.add(new Employee(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
+		EMPLOYEE_List.add(new Employee(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
+		EMPLOYEE_List.add(new Employee(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
 
-		Optional<Employee_10> youngestMaleEmployeeInSalesWrapper = EMPLOYEE_List.stream()
+		Optional<Employee> youngestMaleEmployeeInSalesWrapper = EMPLOYEE_List.stream()
 				.filter(employee -> employee.getGender() == Gender.MALE && employee.getDepartment() == Department.SALES)
-				.min(Comparator.comparingInt(Employee_10::getAge));
+				.min(Comparator.comparingInt(Employee::getAge));
 
-		Employee_10 youngestMaleEmployeeInSalesDepartment = youngestMaleEmployeeInSalesWrapper.get();
+		Employee youngestMaleEmployeeInSalesDepartment = youngestMaleEmployeeInSalesWrapper.get();
 
 		System.out.println("ID - " + youngestMaleEmployeeInSalesDepartment.getId());
 		System.out.println("Name - " + youngestMaleEmployeeInSalesDepartment.getName());
