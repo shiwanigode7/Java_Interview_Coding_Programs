bb120cc622e7299ffd3edde8dd11fee66affd32c
Shiwani Gode <41783218+shiwanigode7@users.noreply.github.com>
2023-08-14 21:22:20 +0530

    Add files via upload

---
+++ Employee_7.java
@@ -0,0 +1,90 @@
+package com.interview.problems.java.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// You have given list of employees, find out the names of all employee who have joined after 2015
+public class Employee_7 {
+
+	private int id;
+	private String name;
+	private int age;
+	private Gender gender;
+	private Department department;
+	private int yearOfJoining;
+	private double salary;
+
+	public Employee_7(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
+			double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.department = department;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public int getYearOfJoining() {
+		return yearOfJoining;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee_7 [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]";
+	}
+
+	private static List<Employee_7> EMPLOYEE_List = new ArrayList<Employee_7>();
+
+	public static void main(String[] args) {
+
+		EMPLOYEE_List.add(new Employee_7(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
+		EMPLOYEE_List.add(new Employee_7(2, "Mithun", 15, Gender.MALE, Department.FINANCE, 2000, 40000));
+		EMPLOYEE_List.add(new Employee_7(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
+		EMPLOYEE_List.add(new Employee_7(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
+		EMPLOYEE_List.add(new Employee_7(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
+		EMPLOYEE_List.add(new Employee_7(6, "shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
+		EMPLOYEE_List.add(new Employee_7(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
+		EMPLOYEE_List.add(new Employee_7(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
+		EMPLOYEE_List.add(new Employee_7(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
+		EMPLOYEE_List.add(new Employee_7(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
+
+		EMPLOYEE_List.stream().filter(employee -> employee.getYearOfJoining() > 2015).map(Employee_7::getName)
+				.forEach(System.out::println);
+	}
+
+}
+
+enum Gender {
+	MALE, FEMALE
+}
+
+enum Department {
+	MARKETING, HR, SALES, FINANCE, RND, IT, MANUFACTURING, QUALITY_MANAGEMENT
+}
