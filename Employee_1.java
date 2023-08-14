3e87a6e4b927f922c51e20abab3f63f184df5542
Shiwani Gode <41783218+shiwanigode7@users.noreply.github.com>
2023-08-14 22:23:05 +0530

    Update Employee_1.java

--- Employee_1.java
+++ Employee_1.java
@@ -43,20 +43,20 @@ public class Employee {
 		return "Employee [name=" + name + ", age=" + age + ", dob=" + dob + ", salary=" + salary + "]";
 	}
 
-	private static List<Employee> EMPLOYEE_List = new ArrayList<Employee>();
+	private static List<Employee> EMPLOYEE_LIST = new ArrayList<Employee>();
 
 	public static void main(String[] args) {
 
-		EMPLOYEE_List.add(new Employee("Shiwani", 31, LocalDate.parse("1992-07-21"), 3000));
-		EMPLOYEE_List.add(new Employee("Prasad", 62, LocalDate.parse("1998-01-29"), 2000));
-		EMPLOYEE_List.add(new Employee("Mithun", 50, LocalDate.parse("2002-04-04"), 7000));
-		EMPLOYEE_List.add(new Employee("Emma", 24, LocalDate.parse("2010-06-11"), 65000));
-		EMPLOYEE_List.add(new Employee("Parag", 26, LocalDate.parse("1995-08-01"), 46000));
-		EMPLOYEE_List.add(new Employee("Kishor", 32, LocalDate.parse("2023-09-19"), 12000));
-		EMPLOYEE_List.add(new Employee("Wayu", 40, LocalDate.parse("2004-02-13"), 50000));
-		EMPLOYEE_List.add(new Employee("Om", 45, LocalDate.parse("2015-01-09"), 80000));
+		EMPLOYEE_LIST.add(new Employee("Shiwani", 31, LocalDate.parse("1992-07-21"), 3000));
+		EMPLOYEE_LIST.add(new Employee("Prasad", 62, LocalDate.parse("1998-01-29"), 2000));
+		EMPLOYEE_LIST.add(new Employee("Mithun", 50, LocalDate.parse("2002-04-04"), 7000));
+		EMPLOYEE_LIST.add(new Employee("Emma", 24, LocalDate.parse("2010-06-11"), 65000));
+		EMPLOYEE_LIST.add(new Employee("Parag", 26, LocalDate.parse("1995-08-01"), 46000));
+		EMPLOYEE_LIST.add(new Employee("Kishor", 32, LocalDate.parse("2023-09-19"), 12000));
+		EMPLOYEE_LIST.add(new Employee("Wayu", 40, LocalDate.parse("2004-02-13"), 50000));
+		EMPLOYEE_LIST.add(new Employee("Om", 45, LocalDate.parse("2015-01-09"), 80000));
 
-		Map<Integer, List<Employee>> result = EMPLOYEE_List.stream()
+		Map<Integer, List<Employee>> result = EMPLOYEE_LIST.stream()
 				.collect(Collectors.groupingBy(
 						employee -> employee.dob.compareTo(LocalDate.parse("2000-01-01")) < 0 ? -1
 								: (employee.dob.compareTo(LocalDate.parse("2000-01-01")) == 0 ? 0 : 1)));
