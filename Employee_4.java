83bef89d3e7c8314be2020bfca2b04dbbbcbf54a
Shiwani Gode <41783218+shiwanigode7@users.noreply.github.com>
2023-08-14 18:26:44 +0530

    Update Employee_4.java

--- Employee_4.java
+++ Employee_4.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 // You have given list of employees, print the name of all departments in the organization
-public class Employee_4 {
+public class Employee {
 
 	private int id;
 	private String name;
@@ -14,7 +14,7 @@ public class Employee_4 {
 	private int yearOfJoining;
 	private double salary;
 
-	public Employee_4(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
+	public Employee(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
 			double salary) {
 		super();
 		this.id = id;
@@ -56,26 +56,26 @@ public class Employee_4 {
 
 	@Override
 	public String toString() {
-		return "Employee_4 [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
 				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]";
 	}
 
-	private static List<Employee_4> EMPLOYEE_List = new ArrayList<Employee_4>();
+	private static List<Employee> EMPLOYEE_List = new ArrayList<Employee>();
 
 	public static void main(String[] args) {
 
-		EMPLOYEE_List.add(new Employee_4(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
-		EMPLOYEE_List.add(new Employee_4(2, "Mithun", 15, Gender.MALE, Department.FINANCE, 2000, 40000));
-		EMPLOYEE_List.add(new Employee_4(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
-		EMPLOYEE_List.add(new Employee_4(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
-		EMPLOYEE_List.add(new Employee_4(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
-		EMPLOYEE_List.add(new Employee_4(6, "shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
-		EMPLOYEE_List.add(new Employee_4(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
-		EMPLOYEE_List.add(new Employee_4(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
-		EMPLOYEE_List.add(new Employee_4(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
-		EMPLOYEE_List.add(new Employee_4(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
+		EMPLOYEE_List.add(new Employee(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
+		EMPLOYEE_List.add(new Employee(2, "Mithun", 15, Gender.MALE, Department.FINANCE, 2000, 40000));
+		EMPLOYEE_List.add(new Employee(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
+		EMPLOYEE_List.add(new Employee(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
+		EMPLOYEE_List.add(new Employee(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
+		EMPLOYEE_List.add(new Employee(6, "shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
+		EMPLOYEE_List.add(new Employee(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
+		EMPLOYEE_List.add(new Employee(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
+		EMPLOYEE_List.add(new Employee(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
+		EMPLOYEE_List.add(new Employee(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
 
-		EMPLOYEE_List.stream().map(Employee_4::getDepartment).distinct().forEach(System.out::println);
+		EMPLOYEE_List.stream().map(Employee::getDepartment).distinct().forEach(System.out::println);
 	}
 
 }
