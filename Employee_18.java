9945d2e7d7d29ef17649764047076146684518a8
Shiwani Gode <41783218+shiwanigode7@users.noreply.github.com>
2023-08-15 15:56:54 +0530

    Update Employee_18.java

--- Employee_18.java
+++ Employee_18.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 
 // You have given list of employees, sort the employees by name and salary in ascending order
-public class Employee_18 {
+public class Employee {
 
 	private int id;
 	private String name;
@@ -15,7 +15,7 @@ public class Employee_18 {
 	private int yearOfJoining;
 	private double salary;
 
-	public Employee_18(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
+	public Employee(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
 			double salary) {
 		super();
 		this.id = id;
@@ -57,37 +57,37 @@ public class Employee_18 {
 
 	@Override
 	public String toString() {
-		return "Employee_18 [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
 				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]";
 	}
 
-	private static List<Employee_18> EMPLOYEE_LIST = new ArrayList<Employee_18>();
+	private static List<Employee> EMPLOYEE_LIST = new ArrayList<Employee>();
 
 	public static void main(String[] args) {
 
-		EMPLOYEE_LIST.add(new Employee_18(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
-		EMPLOYEE_LIST.add(new Employee_18(2, "Mithun", 15, Gender.MALE, Department.IT, 2000, 40000));
-		EMPLOYEE_LIST.add(new Employee_18(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
-		EMPLOYEE_LIST.add(new Employee_18(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
-		EMPLOYEE_LIST.add(new Employee_18(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
-		EMPLOYEE_LIST.add(new Employee_18(6, "Shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
-		EMPLOYEE_LIST.add(new Employee_18(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
-		EMPLOYEE_LIST.add(new Employee_18(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
-		EMPLOYEE_LIST.add(new Employee_18(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
-		EMPLOYEE_LIST.add(new Employee_18(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
+		EMPLOYEE_LIST.add(new Employee(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
+		EMPLOYEE_LIST.add(new Employee(2, "Mithun", 15, Gender.MALE, Department.IT, 2000, 40000));
+		EMPLOYEE_LIST.add(new Employee(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
+		EMPLOYEE_LIST.add(new Employee(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
+		EMPLOYEE_LIST.add(new Employee(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
+		EMPLOYEE_LIST.add(new Employee(6, "Shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
+		EMPLOYEE_LIST.add(new Employee(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
+		EMPLOYEE_LIST.add(new Employee(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
+		EMPLOYEE_LIST.add(new Employee(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
+		EMPLOYEE_LIST.add(new Employee(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
 
-		EMPLOYEE_LIST.sort(Comparator.comparingDouble(Employee_18::getSalary));
+		EMPLOYEE_LIST.sort(Comparator.comparingDouble(Employee::getSalary));
 
 		System.out.println("Sorted By Salary");
-		for (Employee_18 employee : EMPLOYEE_LIST) {
+		for (Employee employee : EMPLOYEE_LIST) {
 			System.out.println(employee.getName() + " - " + employee.getSalary());
 		}
 		System.out.println("=============================================");
 
-		EMPLOYEE_LIST.sort(Comparator.comparing(Employee_18::getName));
+		EMPLOYEE_LIST.sort(Comparator.comparing(Employee::getName));
 
 		System.out.println("Sorted By Name");
-		for (Employee_18 employee : EMPLOYEE_LIST) {
+		for (Employee employee : EMPLOYEE_LIST) {
 			System.out.println(employee.getName() + " - " + employee.getSalary());
 		}
 	}
