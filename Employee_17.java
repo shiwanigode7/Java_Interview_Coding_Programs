c5be2f44873bbc054e397349de56ce633d18b82a
Shiwani Gode <41783218+shiwanigode7@users.noreply.github.com>
2023-08-15 15:16:25 +0530

    Add files via upload

---
+++ Employee_17.java
@@ -0,0 +1,96 @@
+package com.interview.problems.java.core;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+// You have given list of employees, find out who is oldest employee in the organization? what is his/her age? and which department he belongs to?
+public class Employee_17 {
+
+	private int id;
+	private String name;
+	private int age;
+	private Gender gender;
+	private Department department;
+	private int yearOfJoining;
+	private double salary;
+
+	public Employee_17(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
+			double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.department = department;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public int getYearOfJoining() {
+		return yearOfJoining;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee_17 [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]";
+	}
+
+	private static List<Employee_17> EMPLOYEE_LIST = new ArrayList<Employee_17>();
+
+	public static void main(String[] args) {
+
+		EMPLOYEE_LIST.add(new Employee_17(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
+		EMPLOYEE_LIST.add(new Employee_17(2, "Mithun", 15, Gender.MALE, Department.IT, 2000, 40000));
+		EMPLOYEE_LIST.add(new Employee_17(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
+		EMPLOYEE_LIST.add(new Employee_17(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
+		EMPLOYEE_LIST.add(new Employee_17(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
+		EMPLOYEE_LIST.add(new Employee_17(6, "shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
+		EMPLOYEE_LIST.add(new Employee_17(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
+		EMPLOYEE_LIST.add(new Employee_17(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
+		EMPLOYEE_LIST.add(new Employee_17(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
+		EMPLOYEE_LIST.add(new Employee_17(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
+
+		Optional<Employee_17> oldestEmployeeWrapper = EMPLOYEE_LIST.stream()
+				.max(Comparator.comparingInt(Employee_17::getAge));
+		Employee_17 oldestEmployee = oldestEmployeeWrapper.get();
+
+		System.out.println("Name: " + oldestEmployee.getName());
+		System.out.println("Age: " + oldestEmployee.getAge());
+		System.out.println("Department: " + oldestEmployee.getDepartment());
+	}
+}
+
+enum Gender {
+	MALE, FEMALE
+}
+
+enum Department {
+	MARKETING, HR, SALES, FINANCE, RND, IT, MANUFACTURING, QUALITY_MANAGEMENT
+}
