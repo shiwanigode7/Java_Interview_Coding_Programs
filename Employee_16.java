07a9a90ad2a4203d8d7f49e79928b81a89cf9fe5
Shiwani Gode <41783218+shiwanigode7@users.noreply.github.com>
2023-08-15 15:16:10 +0530

    Update Employee_16.java

--- Employee_16.java
+++ Employee_16.java
@@ -8,7 +8,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 // You have given list of employees, separate the employees who are younger or equal to 25 years from those employees who are older than 25 years
-public class Employee_16 {
+public class Employee {
 
 	private int id;
 	private String name;
@@ -18,7 +18,7 @@ public class Employee_16 {
 	private int yearOfJoining;
 	private double salary;
 
-	public Employee_16(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
+	public Employee(int id, String name, int age, Gender gender, Department department, int yearOfJoining,
 			double salary) {
 		super();
 		this.id = id;
@@ -60,30 +60,30 @@ public class Employee_16 {
 
 	@Override
 	public String toString() {
-		return "Employee_16 [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
 				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]";
 	}
 
-	private static List<Employee_16> EMPLOYEE_LIST = new ArrayList<Employee_16>();
+	private static List<Employee> EMPLOYEE_LIST = new ArrayList<Employee>();
 
 	public static void main(String[] args) {
 
-		EMPLOYEE_LIST.add(new Employee_16(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
-		EMPLOYEE_LIST.add(new Employee_16(2, "Mithun", 15, Gender.MALE, Department.IT, 2000, 40000));
-		EMPLOYEE_LIST.add(new Employee_16(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
-		EMPLOYEE_LIST.add(new Employee_16(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
-		EMPLOYEE_LIST.add(new Employee_16(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
-		EMPLOYEE_LIST.add(new Employee_16(6, "shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
-		EMPLOYEE_LIST.add(new Employee_16(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
-		EMPLOYEE_LIST.add(new Employee_16(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
-		EMPLOYEE_LIST.add(new Employee_16(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
-		EMPLOYEE_LIST.add(new Employee_16(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
+		EMPLOYEE_LIST.add(new Employee(1, "Shiwani", 31, Gender.FEMALE, Department.IT, 2018, 80000));
+		EMPLOYEE_LIST.add(new Employee(2, "Mithun", 15, Gender.MALE, Department.IT, 2000, 40000));
+		EMPLOYEE_LIST.add(new Employee(3, "Prasad", 18, Gender.MALE, Department.HR, 2006, 2000));
+		EMPLOYEE_LIST.add(new Employee(4, "Shwetangi", 20, Gender.FEMALE, Department.RND, 2010, 88000));
+		EMPLOYEE_LIST.add(new Employee(5, "Vijay", 16, Gender.MALE, Department.SALES, 2022, 3000));
+		EMPLOYEE_LIST.add(new Employee(6, "shaila", 45, Gender.FEMALE, Department.MARKETING, 2021, 90000));
+		EMPLOYEE_LIST.add(new Employee(7, "Dhirendra", 25, Gender.MALE, Department.SALES, 2015, 100000));
+		EMPLOYEE_LIST.add(new Employee(8, "Hanamanth", 59, Gender.MALE, Department.SALES, 2009, 5000));
+		EMPLOYEE_LIST.add(new Employee(9, "Om", 45, Gender.MALE, Department.MANUFACTURING, 2003, 13000));
+		EMPLOYEE_LIST.add(new Employee(10, "Liana", 23, Gender.FEMALE, Department.IT, 2023, 9000));
 
-		Map<Boolean, List<Employee_16>> partitionEmployeesByAge = EMPLOYEE_LIST.stream()
+		Map<Boolean, List<Employee>> partitionEmployeesByAge = EMPLOYEE_LIST.stream()
 				.collect(Collectors.partitioningBy(employee -> employee.getAge() > 25));
 
-		Set<Entry<Boolean, List<Employee_16>>> entrySet = partitionEmployeesByAge.entrySet();
-		for (Entry<Boolean, List<Employee_16>> entry : entrySet) {
+		Set<Entry<Boolean, List<Employee>>> entrySet = partitionEmployeesByAge.entrySet();
+		for (Entry<Boolean, List<Employee>> entry : entrySet) {
 
 			System.out.println("======================================");
 
@@ -95,9 +95,9 @@ public class Employee_16 {
 
 			System.out.println("======================================");
 
-			List<Employee_16> employeeList = entry.getValue();
+			List<Employee> employeeList = entry.getValue();
 
-			for (Employee_16 employee : employeeList) {
+			for (Employee employee : employeeList) {
 				System.out.println(employee.getName());
 			}
 		}
